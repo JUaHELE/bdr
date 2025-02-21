91dc87e1a196d26693ea6641056858b33b70eb22
Hynek <hynek2002@gmail.com>
2025-02-21 16:19:52 +0100

    minor changes

--- user/go/client/main.go
+++ user/go/client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bdr/networking"
+	"bdr/utils"
+	"bdr/pause"
 	"encoding/gob"
 	"fmt"
 	"log"
@@ -11,7 +13,6 @@ import (
 	"sync"
 	"syscall"
 	"unsafe"
-	"bdr/utils"
 )
 
 type atomic_t int32
@@ -36,14 +37,15 @@ type TargetInfo struct {
 }
 
 type Client struct {
-	Config       *Config
-	TargetInfo   *TargetInfo
-	Conn         net.Conn
-	Encoder      *gob.Encoder
-	Decoder      *gob.Decoder
-	UnderDevFile *os.File
-	CharDevFile  *os.File // Path to character device which is used for communication with kernel
-	Buf          []byte   // Shared buffer between kernel and userspace where writes will be saved
+	Config            *Config
+	TargetInfo        *TargetInfo
+	Conn              net.Conn
+	Encoder           *gob.Encoder
+	Decoder           *gob.Decoder
+	UnderDevFile      *os.File
+	CharDevFile       *os.File         // Path to character device which is used for communication with kernel
+	Buf               []byte           // Shared buffer between kernel and userspace where writes will be saved
+	MonitorPauseContr *pause.PauseController // used to pause monitor changes function
 }
 
 var (
@@ -106,14 +108,17 @@ func NewClient(cfg *Config) (*Client, error) {
 		return nil, fmt.Errorf("failed to open target device: %w", err)
 	}
 
+	monitorPauseContr := pause.NewPauseController()
+
 	return &Client{
-		Config:       cfg,
-		Conn:         conn,
-		Encoder:      encoder,
-		Decoder:      decoder,
-		UnderDevFile: underDevFd,
-		CharDevFile:  charDevFd,
-		Buf:          buf,
+		Config:            cfg,
+		Conn:              conn,
+		Encoder:           encoder,
+		Decoder:           decoder,
+		UnderDevFile:      underDevFd,
+		CharDevFile:       charDevFd,
+		Buf:               buf,
+		MonitorPauseContr: monitorPauseContr,
 	}, nil
 }
 
@@ -135,19 +140,20 @@ func (c *Client) Close() {
 func (c *Client) MonitorChanges(termChan <-chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// reader := bytes.NewReader(nil)
+	pauseController := c.MonitorPauseContr
 
 	for {
-		select {
-		case <-termChan:
+		if terminated := pauseController.WaitIfPaused(termChan); terminated {
 			c.VerbosePrintln("Stopping change monitoring due to shutdown.")
 			return
-		default:
-			var bufferInfo BufferInfo
-			err := ioctl(c.CharDevFile.Fd(), BDR_CMD_GET_BUFFER_INFO_WAIT, uintptr(unsafe.Pointer(&bufferInfo)))
-			if err != nil {
-				log.Printf("ioctl syscall failed: %v", err)
-			}
 		}
+
+		var bufferInfo BufferInfo
+		err := ioctl(c.CharDevFile.Fd(), BDR_CMD_GET_BUFFER_INFO_WAIT, uintptr(unsafe.Pointer(&bufferInfo)))
+		if err != nil {
+			log.Printf("ioctl syscall failed: %v", err)
+		}
+
 	}
 }
 
@@ -177,13 +183,14 @@ func main() {
 		log.Fatalf("Invalid arguments: %v", err)
 	}
 
-	sender, err := NewClient(cfg)
+	client, err := NewClient(cfg)
 	if err != nil {
 		log.Fatalf("Failed to initiate sender: %v", err)
 	}
-	defer sender.Close()
+	defer client.Close()
 
 	// register needed packets
 	networking.RegisterGobPackets()
 
+	client.Run()
 }
