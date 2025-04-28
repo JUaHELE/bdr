166b625d231fa7d2fb1af9e64c92f2f4bf89e6c0
Hynek <hynek2002@gmail.com>
2025-04-28 16:16:24 +0200

    minor changes

--- kern/.bdr.o.cmd
+++
@@ -1,3 +0,0 @@
-savedcmd_bdr.o := ld -m elf_x86_64 -z noexecstack --no-warn-rwx-segments   -r -o bdr.o @bdr.mod  ; /usr/lib/modules/6.13.8-arch1-1/build/tools/objtool/objtool --hacks=jump_label --hacks=noinstr --hacks=skylake --ibt --orc --retpoline --rethunk --sls --static-call --uaccess --prefix=16  --link  --module bdr.o
-
-bdr.o: $(wildcard /usr/lib/modules/6.13.8-arch1-1/build/tools/objtool/objtool)
